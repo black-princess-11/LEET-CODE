d442128b2fd6aca70d199a943c631fd7e3fd9f83
vishva Hadvani <147624128+vishva0311@users.noreply.github.com>
2026-01-25 16:54:57 +0530

    Add Some Patterns

--- Program_1.py
+++ Program_1.py
@@ -1,5 +1,21 @@
 print("Hello, World!")
-r=int(input("Enter a number: "))
+r=int(input("Enter a number of row you want to display in pattern: "))
+que=input("Do you want to print which pattern?[1. Right angled triangle 2. Reversed right angled triangle 3. Dimond 4. Center Tringle]: ")
 i=0
-for i in range(i,r+1):
-    print("*"*i,)
+if que=="Right angled triangle"or que=="right angled triangle":
+    for i in range(i,r+1):
+        print("*"*i,)
+elif que=="Reversed right angled triangle"or que=="reversed right angled triangle":
+    for i in range(r,0,-1):
+        print("*"*i,)
+elif que=="Dimond" or que=="dimond":
+    for i in range(r):
+        print(" "*(r-i-1)+"*"*(2*i+1))
+    for i in range(r-2,-1,-1):
+        print(" "*(r-i-1)+"*"*(2*i+1))
+elif que=="Center Tringle"or que=="center tringle":
+    for i in range(r):
+        print(" "*(r-i-1),"*"*(2*i+1))
+else:
+    for i in range(i,r+1):
+        print("Hi!! "*i,(" "*r))
